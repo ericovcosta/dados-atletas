2a813863f5b4e4fb42dfc0922d8db80f7efc340a
ericovinicosta <professor@ericocosta.com.br>
2024-07-28 23:28:19 -0300

    c1

---
+++ dados-atletas/js/atletas.js
@@ -0,0 +1,52 @@
+const atleta = new Atletas(
+    "Cesar Abascal",30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]
+);
+
+class Atletas{
+    constructor(nome, idade, peso, altura, notas ){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+    //  para calcular a categoria do atleta;
+    calculaCategoria(){
+    }
+    /*para calcular o IMC do atleta;*/
+    calculaIMC(){
+    }
+    /* para calcular a média válida do atleta.*/
+    calculaMediaValida(){
+
+    }
+    /* que retorna o nome do atleta*/
+    obtemNomeAtleta(){
+
+    }
+    /*que retorna a idade do atleta*/
+    obtemIdadeAtleta(){
+
+    }
+    /* que retorna o peso do atleta*/
+    obtemPesoAtleta(){
+
+    }
+    /* que retorna as notas do atleta*/
+    obtemNotasAtleta(){
+
+    }
+    /*que retorna a categoria do atleta*/
+    obtemCategoria(){
+
+    }
+    /*que retorna o IMC do atleta*/
+    obtemIMC(){
+
+    }
+    /*que retorna a média válida do atleta*/
+    obtemMediaValida(){
+
+    }
+}
